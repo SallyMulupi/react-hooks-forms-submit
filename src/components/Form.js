32d76588c54b5a626dd24b7b90de48e69ce8e3d0
SallyMulupi <mulupisally2gmail.com>
2022-07-22 01:56:28 +0300

    add const submitteddata

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,32 @@
 import React, { useState } from "react";
 
 function Form(props) {
-  const [firstName, setFirstName] = useState("Sylvia");
-  const [lastName, setLastName] = useState("Woods");
+    const [firstName, setFirstName] = useState("Sylvia");
+    const [lastName, setLastName] = useState("Woods");
+    const [submittedData, setSubmittedData] = useState([]);
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
-  }
+    function handleFirstNameChange(event) {
+        setFirstName(event.target.value);
+    }
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
-  }
+    function handleLastNameChange(event) {
+        setLastName(event.target.value);
+    }
 
-  return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
-  );
+    return ( <
+        form >
+        <
+        input type = "text"
+        onChange = { handleFirstNameChange }
+        value = { firstName }
+        /> <
+        input type = "text"
+        onChange = { handleLastNameChange }
+        value = { lastName }
+        /> <
+        button type = "submit" > Submit < /button> <
+        /form>
+    );
 }
 
 export default Form;
